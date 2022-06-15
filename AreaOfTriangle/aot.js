f9d8d94b4a2841a9f5a730683faf9a5a38dc528b
GarlicBuns19 <planterx26@gmail.com>
2022-06-15 14:16:33 +0200

    done

--- AreaOfTriangle/aot.js
+++ AreaOfTriangle/aot.js
@@ -4,5 +4,7 @@ c.addEventListener('click', () => {
     let b = document.getElementById('base').value
     let h = document.getElementById('height').value
 
-    
+    let answer = eval(1/2 * (b*h))
+
+    document.getElementById('answer').innerHTML = answer;
 })
