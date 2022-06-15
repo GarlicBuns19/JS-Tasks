f206b0324279c1995a6d614bb5ca1e80cafda85b
GarlicBuns19 <planterx26@gmail.com>
2022-06-15 16:12:15 +0200

    done

--- Age to days/ageToDays.js
+++ Age to days/ageToDays.js
@@ -5,7 +5,14 @@ c.addEventListener('click',() => {
     let age = document.getElementById('age').value
     let days = age*365;
     document.getElementById('days').innerHTML = days;
-    let hours = document.getElementById('hours')
-    let minutes = document.getElementById('minutes')
-    let seconds = document.getElementById('days')
+    let hours = age*365*24
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('hours1').innerHTML = ' hrs';
+    let minutes = age*365*24*60
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('minutes1').innerHTML = ' min';
+    let seconds = age*365*24*60
+    document.getElementById('seconds').innerHTML = seconds;
+    document.getElementById('seconds1').innerHTML = ' s';
+
 })
