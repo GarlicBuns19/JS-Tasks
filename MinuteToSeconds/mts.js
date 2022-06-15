678638a441a91fed4a24f3b9f74c36875ce8ae36
GarlicBuns19 <planterx26@gmail.com>
2022-06-15 09:28:49 +0200

    done

--- MinuteToSeconds/mts.js
+++ MinuteToSeconds/mts.js
@@ -1,3 +1,14 @@
 let minute = document.querySelector('#minute');
-let seconds = document.getElementById('seconds')
+let seconds = document.getElementById('seconds');
+
 let calculate = document.getElementById('calculate');
+let c = calculate;
+
+c.addEventListener('click',() => {
+    let m = minute.value;
+    let s = seconds.value;
+
+    answer = eval(m * 60)
+    document.getElementById('output').innerHTML = answer;
+})
+
