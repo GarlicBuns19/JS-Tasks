0d2addcc6a9d197cedcf978d8df37fdbfb743a41
GarlicBuns19 <planterx26@gmail.com>
2022-06-15 14:10:54 +0200

    help

---
+++ AreaOfTriangle/aot.js
@@ -0,0 +1,8 @@
+let c = document.getElementById('calculate');
+
+c.addEventListener('click', () => {
+    let b = document.getElementById('base').value
+    let h = document.getElementById('height').value
+
+    
+})
