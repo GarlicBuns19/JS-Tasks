a5c679abb8010a4f28cb6fce4f757b10bbfb9a27
GarlicBuns19 <planterx26@gmail.com>
2022-06-19 12:48:46 +0100

    help

--- addUp/addUp.js
+++ addUp/addUp.js
@@ -1,7 +1,18 @@
 const numbers = document.getElementById('numbers');
-const btncal = document.getElementById('cal');
+const btnaddNum = document.getElementById('addNum');
+const numsToCal = document.getElementById('numsToCal');
 const arr = [];
 
-btncal.addEventListener('click', () => {
+btnaddNum.addEventListener('click', () => {
     
+    let i = arr.length
+    arr[i] = parseInt(numbers.value)
+
+    if(i >= 1){
+        numsToCal.innerHTML += ', '
+    }
+
+    numsToCal.innerHTML += arr[i]
 })
+
+// Calculate Array of Numbers
