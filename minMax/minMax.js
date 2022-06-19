de32c7b70923284fd906744354ea5410c5a908b5
GarlicBuns19 <planterx26@gmail.com>
2022-06-19 11:40:34 +0100

    help

--- minMax/minMax.js
+++ minMax/minMax.js
@@ -1,8 +1,18 @@
-const inputNum = document.querySelector('#inputNum')
 const arr = []
+const array = document.getElementById('array')
 
 // Enter into Array
 const btnAdd = document.querySelector('#btnAdd')
 btnAdd.addEventListener('click' , (x) => {
-
+    x = document.getElementById('inputNum').value ;
+    let i = arr.length ;
+    arr[i] = x ;
+    
+    if (i >= 1) {
+        array.innerHTML += ', '
+    }
+    array.innerHTML += arr[i] ;
+    i++;
+    document.getElementById('arr').value = '';
+    document.getElementById('arr').focus()
 })
