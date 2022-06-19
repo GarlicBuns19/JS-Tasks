eb49887cdfb9b4c0cc292ff76a20f79f347c337d
GarlicBuns19 <planterx26@gmail.com>
2022-06-19 11:11:25 +0100

    help

--- minMax/minMax.js
+++ minMax/minMax.js
@@ -1,1 +1,8 @@
+const inputNum = document.querySelector('#inputNum')
+const arr = []
 
+// Enter into Array
+const btnAdd = document.querySelector('#btnAdd')
+btnAdd.addEventListener('click' , (x) => {
+
+})
