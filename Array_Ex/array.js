3d555833392e9a3b4af2acf4108f63ed279d32ae
GarlicBuns19 <planterx26@gmail.com>
2022-06-20 13:37:19 +0200

    help

--- Array_Ex/array.js
+++ Array_Ex/array.js
@@ -28,3 +28,8 @@ document.getElementById('show5').innerHTML = data
 
 let arr = data.slice(0,2)
 document.getElementById('show6').innerHTML = arr
+
+let arr1 = [1,2,3,4,5,6]
+document.getElementById('show7').innerHTML = arr1.indexOf(1)
+document.getElementById('show8').innerHTML = arr1.indexOf('1')
+document.getElementById('show9').innerHTML = arr1.lastIndexOf(1 )
